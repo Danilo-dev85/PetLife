5ecf28576328fcce9805813e72a3f1a859774ee2
Danilo Ribeiro <daniloemirele84@gmail.com>
2024-02-23 17:24:45 -0300

    full application

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const elementosDuvida = document.querySelectorAll(".duvida");
+
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", () => {
+    duvida.classList.toggle("ativa");
+  });
+});
